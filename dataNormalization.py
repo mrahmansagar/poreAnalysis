7864c285038fe2e4956df93d3ce6dfc1511b1d84
mrahmansagar <mrahman.sagar@gmail.com>
2022-07-22 17:44:33 +0200

    data normalization with global min max value

---
+++ dataNormalization.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 22 15:16:03 2022
+
+@author: Mmr Sagar
+PhD Student | AG Alves 
+MPI for Multidisciplinary Sciences, Germany 
+
+Investigation for setting a range for convertiong the data into a specific data 
+type
+
+"""
+
