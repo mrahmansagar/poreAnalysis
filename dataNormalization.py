c1c5c7e74d8b2751e9fce1070a83db6e7144832e
mrahmansagar <mrahman.sagar@gmail.com>
2022-07-25 17:44:28 +0200

    saving the file as raw with numpy tofile

--- dataNormalization.py
+++ dataNormalization.py
@@ -24,14 +24,13 @@ from tkinter import Tcl
 
 
 # My utilies 
-import poreUtils 
+from poreUtils import * 
 
 data_dir = 'D:\sagar\Data\MD_1264_A1_1_Z3.3mm\selected_roi'
 selected_files = os.listdir(data_dir)
 
 
 
-roi_info = {}
 mins = []
 maxs = []
 for afile in tqdm(selected_files):
@@ -46,40 +45,10 @@ for afile in tqdm(selected_files):
         
     raw_vol = np.asarray(raw_vol)
     
-    roi_info[afile] = [raw_vol.min(), raw_vol.max()]
     mins.append(raw_vol.min())
     maxs.append(raw_vol.max())
     
-    #raw_vol = poreUtils.norm8bit(raw_vol)
     
-    
-    
-def norm8bit(v, mn, mx):
-    """
-    NORM8BIT function takes an array and normalized it before converting it into 
-    a 8 bit unsigned integer and returns it.
-
-    Parameters
-    ----------
-    v : numpy.ndarray
-        Array of N dimension.
-
-    Returns
-    -------
-    numpy.ndarray (uint8)
-        Numpy Array of same dimension as input with data type as unsigned integer 8 bit
-
-    """
-    
-    #mn = v.min()
-    #mx = v.max()
-      
-    mx -= mn
-      
-    v = ((v - mn)/mx) * 255
-    
-    return v.astype(np.uint8)
-
 
 
 for afile in tqdm(selected_files):
@@ -93,8 +62,8 @@ for afile in tqdm(selected_files):
         raw_vol.append(imgarray)
         
     raw_vol = np.asarray(raw_vol)
-    raw_vol = norm8bit(raw_vol, mn=max(mins), mx=min(maxs))
-    tifffile.imsave((file_path + '_8bit.raw'), raw_vol)
+    raw_vol = norm8bit(raw_vol, minVal=max(mins), maxVal=min(maxs))
+    raw_vol.tofile(file_path + '_8bit.raw')
     
     
     
