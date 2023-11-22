67fb55a291b6f0f2be57aa3bfe4d2b2851fb1f6b
mrahmansagar <mrahman.sagar@gmail.com>
2023-11-22 16:59:49 +0100

    cross correlation of features in a heatmap

--- cross_correlation_heatmap.py
+++ cross_correlation_heatmap.py
@@ -1,35 +1,31 @@
 import os 
-import sys
-sys.path.append('E:\\dev\\')
+os.sys.path.insert(0, 'E:\\dev\\packages')
 
 from glob import glob
-import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
 
 import json
 import pandas as pd
 
-from tqdm import tqdm
 
-
-from processTools import utils
+from proUtils import utils
+from proUtils import plot
 
 
 # Directory where the scans are stored with results 
-root_dir = 'E:\\Data\\sam_data\\'
+root_dir = 'E:\\Data\\sam_data\\new\\'
 
 # geting the group information
 df = pd.read_csv('study_group.csv')
 
-# Getting the scans where pypore3d analysis has been run 
+# Getting the scans where analysis has been run 
 scans = []
 for p in os.listdir(root_dir):
     file_path = os.path.join(root_dir, p, 'porespy')
     if os.path.exists(file_path):
         scans.append(p)
 
-# creating diffrent groupsfrom the available scans
+# creating diffrent groups from the available scans
 scan_groups = {}
 for g in np.unique(df['Group']):
     scan_groups[g] = []
@@ -42,135 +38,78 @@ for scan in scans:
 print(scan_groups)
 
 
-cntrl_vili_scans = scan_groups['Con-VILI']
-bleo_vili_scans = scan_groups['Ble-VILI']
 bleo_scans = scan_groups['Ble']
-
-# Cross-Correlation of features for all scans in each group together 
-listOfFeatures = ['volume', 'sphericity', 'surface_area', 'convex_volume', 'equivalent_diameter_area', 'extent', 'solidity']
-
-for i, agrp in enumerate([cntrl_vili_scans, bleo_vili_scans, bleo_scans]):
-
-    data_dict = {}
-    for af in listOfFeatures:
-        data_value = []
-        for aScan in agrp:
-            fpath = os.path.join(root_dir, aScan, 'porespy')
-            json_files = glob(fpath + '\*' + 'dth12_v2' + '.json')
-
-            # list of featutes in all roi in a scan. example [ median(roi1_volume), median(roi2_volume), .........]
-            parameter = [] 
-            for json_file in json_files:
-                file = open(json_file)
-                data = json.load(file)
-                file.close()
-
-                extract_feature = data[af]
-
-                if len(extract_feature) > 0:
-                    parameter.append(np.median(extract_feature))
-            
-            # value appended per scan
-            data_value.append(np.mean(parameter))
-
-        data_dict[af] = data_value
+bleo_vili_scans = scan_groups['Ble-VILI']
+con_scans = scan_groups['Con']
+cntrl_vili_scans = scan_groups['Con-VILI']
 
 
-    data_value = []
-    for aScan in agrp:
-        fpath = os.path.join(root_dir, aScan, 'porespy')
-        json_files = glob(fpath + '\*' + 'dth12' + '.json')
+file_group = con_scans
+file_regx = 'dth8.json'
 
-        # list of featutes in all roi in a scan. example [ median(roi1_volume), median(roi2_volume), .........]
-        parameter = [] 
-        for json_file in json_files:
-            file = open(json_file)
-            data = json.load(file)
-            file.close()
+# Cross-Correlation of features for all scans in each group 
+listOfFeatures = ['volume', 'sphericity', 'surface_area', 'convex_volume', 
+                  'equivalent_diameter_area', 'extent', 'solidity', 'big_pore_ratio']
+
+# initialized data dictionary with an empty list with each feature as a key
+data_dict = {feature: [] for feature in listOfFeatures}
+
+
+json_files = []
+for aScan in file_group:
+    fpath = os.path.join(root_dir, aScan, 'porespy')
+    json_files += glob(fpath + '\*' + file_regx)
+
+
+for afile in json_files:
+    f = open(afile)
+    data = json.load(f)
+    f.close()
+       
+    for feature in listOfFeatures:
+        if feature == 'big_pore_ratio':
             extract_feature = data['volume']
-
             if len(extract_feature) > 0:
-                parameter.append(utils.ratio_above_threshold(extract_feature, 100000))
-        
-        # value appended per scan
-        data_value.append(np.mean(parameter))
+                data_dict[feature].append(utils.ratio_above_threshold(extract_feature, 100000))
+        else:
+            extract_feature = data[feature]
+            if len(extract_feature) > 0:
+                data_dict[feature].append(np.mean(extract_feature))
+    
 
-    data_dict['Big blob ratio'] = data_value
-
-    # create a pandas dataframe from the dictionary
-    df = pd.DataFrame(data_dict)
-
-    # calculate the correlation matrix
-    corr_matrix = df.corr()
-
-    fig = plt.figure(i)
-    # plot the heatmap
-    sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
-   
-    plt.tight_layout()
-    # plt.savefig('heatmap_'+str(i)+'.svg', bbox_inches="tight")
-plt.show()
-
+# changing the big feature name to make the plot look better
+data_dict['equ_dia_area'] = data_dict.pop('equivalent_diameter_area')
+plot.cross_corr_heatmap(data_dict)
 
 
 # Cross-Correlation of features for all scans irrespective of group 
 
-listOfFeatures = ['volume', 'sphericity', 'surface_area', 'convex_volume', 'equivalent_diameter_area', 'extent', 'solidity']
+file_group = cntrl_vili_scans + bleo_vili_scans + bleo_scans
+file_regx = 'dth10.json'
 
-
-data_dict = {}
-for af in listOfFeatures:
-    data_value = []
-    for aScan in (cntrl_vili_scans + bleo_vili_scans + bleo_scans):
-        fpath = os.path.join(root_dir, aScan, 'porespy')
-        json_files = glob(fpath + '\*' + 'dth12_v2' + '.json')
-
-        # list of featutes in all roi in a scan. example [ median(roi1_volume), median(roi2_volume), .........]
-        parameter = [] 
-        for json_file in json_files:
-            file = open(json_file)
-            data = json.load(file)
-            file.close()
-
-            extract_feature = data[af]
-
-            if len(extract_feature) > 0:
-                parameter.append(np.median(extract_feature))
-        
-        # value appended per scan
-        data_value.append(np.mean(parameter))
-
-    data_dict[af] = data_value
-
-
-data_value = []
-for aScan in (cntrl_vili_scans + bleo_vili_scans + bleo_scans):
+json_files = []
+for aScan in file_group:
     fpath = os.path.join(root_dir, aScan, 'porespy')
-    json_files = glob(fpath + '\*' + 'dth12' + '.json')
+    json_files += glob(fpath + '\*' + file_regx)
 
-    # list of featutes in all roi in a scan. example [ median(roi1_volume), median(roi2_volume), .........]
-    parameter = [] 
-    for json_file in json_files:
-        file = open(json_file)
-        data = json.load(file)
-        file.close()
-        extract_feature = data['volume']
 
-        if len(extract_feature) > 0:
-            parameter.append(utils.ratio_above_threshold(extract_feature, 100000))
+for afile in json_files:
+    f = open(afile)
+    data = json.load(f)
+    f.close()
+       
+    for feature in listOfFeatures:
+        if feature == 'big_pore_ratio':
+            extract_feature = data['volume']
+            if len(extract_feature) > 0:
+                data_dict[feature].append(utils.ratio_above_threshold(extract_feature, 100000))
+        else:
+            extract_feature = data[feature]
+            if len(extract_feature) > 0:
+                data_dict[feature].append(np.mean(extract_feature))
     
-    # value appended per scan
-    data_value.append(np.mean(parameter))
 
-data_dict['Big blob ratio'] = data_value
+# changing the big feature name to make the plot look better
+data_dict['equ_dia_area'] = data_dict.pop('equivalent_diameter_area')
+plot.cross_corr_heatmap(data_dict)
 
-# create a pandas dataframe from the dictionary
-df = pd.DataFrame(data_dict)
-
-# calculate the correlation matrix
-corr_matrix = df.corr()
-
-# plot the heatmap
-sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
-# plt.savefig('heatmap_all_dth12_v2.svg', bbox_inches="tight")
-plt.show()
